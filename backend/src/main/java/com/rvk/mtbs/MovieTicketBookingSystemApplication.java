bfbb1789affa9af2d028c24b975ba1ba61e5d656
veeraskumar <veeraskumar000@gmail.com>
2026-07-20 23:03:57 +0530

    backend inti

---
+++ backend/src/main/java/com/rvk/mtbs/MovieTicketBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.rvk.mtbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieTicketBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieTicketBookingSystemApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/rvk/mtbs/MovieTicketBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rvk.mtbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieTicketBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
